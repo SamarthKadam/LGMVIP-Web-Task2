62ff765ca989d18c3af151c8032cd59379587460
Samarth Kadam <samarthskadam14@gmail.com>
2022-07-21 12:39:48 +0530

    Adding File

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
 
+import { useContext } from 'react';
+import styles from './App.module.css'
+import Navbar from './components/Navbar'
+import UserSection from './components/UserSection';
+import Value from './Value';
+import Spinner from './components/Spinner';
 function App() {
+  const data=useContext(Value);
+  console.log("Does it render")
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.App}>
+      <Navbar></Navbar>
+      {
+        data.loader?<Spinner/>:''
+      }
+      {
+       !data.loader&&data.userdata?<UserSection></UserSection> :''
+      }
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import DataValue from './DataValue';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+   <DataValue><App /></DataValue>
   </React.StrictMode>
 );
 
